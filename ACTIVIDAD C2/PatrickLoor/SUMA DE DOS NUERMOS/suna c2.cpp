cabb01eb99b4e7e55002a0330b2f49d3526af9d3
~ HEBRAHIM_LOOR ~ <92316765+HEBRAHIM-LOOR7@users.noreply.github.com>
2021-12-17 22:56:59 -0500

    Add files via upload

---
+++ ACTIVIDAD C2/PatrickLoor/SUMA DE DOS NUERMOS/suna c2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std; 
+
+int main () {
+	int x,y,z =0 ;
+	
+	cout<< "digite un numero: "; cin>>x;
+	cout<< "digite un numero: "; cin>>y;
+	
+	z = x + y;
+	
+	cout<< "respuesta es: "<<z<<endl; 
+	
+	
+	
+	return 0;
+}
