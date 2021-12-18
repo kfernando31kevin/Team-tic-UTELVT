b014d5508ccb9b0a0db9ebaf24f8e3ff5e7e7cb8
Kevin Hurtado <92317013+kfernando31kevin@users.noreply.github.com>
2021-12-18 11:42:34 -0500

    Add files via upload

---
+++ ACTIVIDAD C2/KEVIN FERNANDO HURTADO MERA/SUMA DE DOS NUERMOS/SUMA DE DOS NUMEROS.cpp
@@ -0,0 +1,19 @@
+// hola soy kevin hurtado //
+#include<iostream>
+
+using namespace std; 
+
+int main () {
+	int x,y,z =0 ;
+	
+	cout<< "digite un numero: "; cin>>x;
+	cout<< "digite un numero: "; cin>>y;
+	
+	z = x + y;
+	
+	cout<< "respuesta es: "<<z<<endl; 
+	
+	
+	
+	return 0;
+}
