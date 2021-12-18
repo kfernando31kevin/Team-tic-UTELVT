ad8fbf1e79487a0c2418405357b5f33cc0d881a1
Cijel Espinoza .A <92316618+cijel354@users.noreply.github.com>
2021-12-18 15:05:30 -0500

    Add files via upload

---
+++ ACTIVIDAD C2/CijelEspinoza/SUMA DE DOS NUERMOS/main.cpp
@@ -0,0 +1,17 @@
+//Hola este codigo esta hecho por Cijel Espinoza
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int x,y,z=0
+	cout<<"ingrese un numero: ";
+	cin>>x;
+	cout<<"ingrese un numero: ";
+	cin>>y;
+	z=x+y;
+	cout<<"el resultado es: ";<<z<<endl;
+	
+	
+	return 0;
+}
