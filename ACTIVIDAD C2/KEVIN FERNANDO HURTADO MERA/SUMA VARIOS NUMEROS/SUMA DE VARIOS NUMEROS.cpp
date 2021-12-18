a5faf0b64c0e95a9db7bcb1cda083233c0d36afd
Kevin Hurtado <92317013+kfernando31kevin@users.noreply.github.com>
2021-12-18 11:44:14 -0500

    Add files via upload

---
+++ ACTIVIDAD C2/KEVIN FERNANDO HURTADO MERA/SUMA VARIOS NUMEROS/SUMA DE VARIOS NUMEROS.cpp
@@ -0,0 +1,21 @@
+// hola soy kevin hurtado //
+#include<iostream>
+
+using namespace std; 
+int main () {
+	int x,c=0,s=0,n;
+	
+	cout<< "digite un numero: "; cin>>n;
+	cout<< "digite un numero: "; cin>>x; 
+	
+		c = c + 1;
+		s= s+x; 
+		
+		 if (c<n){
+			cout<<s<<endl;
+			
+		}
+	
+	
+	return 0;
+}
