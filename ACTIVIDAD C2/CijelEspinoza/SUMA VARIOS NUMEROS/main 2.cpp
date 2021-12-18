f8134b7bf055ae4dacb7fab819dc07b395620567
Cijel Espinoza .A <92316618+cijel354@users.noreply.github.com>
2021-12-18 14:57:53 -0500

    Add files via upload

---
+++ ACTIVIDAD C2/CijelEspinoza/SUMA VARIOS NUMEROS/main 2.cpp
@@ -0,0 +1,19 @@
+//hola este codigo esta hecho por Cijel Espinoza
+#include <iostream>
+using namespace std;
+
+int main() {
+	int c=0,n;
+	float s=0,x;
+	cout<<"ingresar un numero:";
+	cin>>n;
+	
+		cout<<"ingresar un numero"<<c+1;
+		cin>>x;
+		s=s+1
+	while (c<n){
+	
+	}cout<<"la suma de los elemtos es:"<<s<<endl;
+	
+	return 0;
+}
